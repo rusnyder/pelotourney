d9c12beac25f6cd8ecefcd1a09df4d336c5938e2
Russell Snyder <russ@arceo.ai>
2021-07-12 00:39:41 -0400

    Switched from sqlite to postgres

--- pelotourney/settings.py
+++ pelotourney/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 import structlog
 from dotenv import load_dotenv
 
@@ -84,13 +85,11 @@ WSGI_APPLICATION = "pelotourney.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres@127.0.0.1:5432/pelotourney"
+)
+CONN_MAX_AGE = int(os.getenv("DATABASE_CONN_MAX_AGE", "0"))
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL, conn_max_age=CONN_MAX_AGE)}
 
 
 # Password validation
